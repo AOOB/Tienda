e8c903a03d9d5ee8ffe8d7908f0b68e6fcf2719c
Hector Dorador <zocross.127@gmail.com>
2016-11-13 10:52:52 -0700

    Arreglandola de nuevo

--- app/Http/Controllers/processController.php
+++ app/Http/Controllers/processController.php
@@ -4,17 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\usuarios as User;
-<<<<<<< HEAD
-<<<<<<< HEAD
-use App\products as products;
-=======
-use App\categories;
-use App\products;
->>>>>>> 51adbf8d39fcac10ca89efc99069782843d6bc4c
-=======
 use App\categories;
 use App\products as products;
->>>>>>> cd558fa00a6bf9e64cf29145fe043feadbd1a86a
 use DB;
 
 class processController extends Controller
@@ -35,18 +26,6 @@ class processController extends Controller
   		 }
 
    	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function ProductView($product){ 
-      $dataProduct = products::find($product);
-      $similarProducts = DB::table('products')->where('categories_id','=',$dataProduct->categories_id)->where('id','<>',$product)->limit(3)->get();
-      $productComments = DB::table('comments as c')->join('users as u','u.id','=','c.user_id')->select('u.id','u.image','c.comment')->where('product_id','=',$product)->where('approved','<>',0)->get();
-      
-      return view('sales.productView', compact('dataProduct','similarProducts','productComments'));      
-    }
-=======
-=======
->>>>>>> cd558fa00a6bf9e64cf29145fe043feadbd1a86a
 
     public function showCategories() {
 
@@ -58,28 +37,12 @@ class processController extends Controller
       return view('principal',compact('dataCates','produc'));
     }
 
-
-<<<<<<< HEAD
-   public function favorites($id) {
-    
-    
-      $query="SELECT image,price FROM products ORDER BY sellers";
-      $produc=products::select($query);
-        return view('',compact('',''));
-   }
->>>>>>> 51adbf8d39fcac10ca89efc99069782843d6bc4c
-    
-=======
     public function ProductView($product){ 
       $dataProduct = products::find($product);
       $similarProducts = DB::table('products')->where('categories_id','=',$dataProduct->categories_id)->where('id','<>',$product)->limit(3)->get();
       $productComments = DB::table('comments as c')->join('users as u','u.id','=','c.user_id')->select('u.id','u.image','c.comment')->where('product_id','=',$product)->where('approved','<>',0)->get();
       
       return view('sales.productView', compact('dataProduct','similarProducts','productComments'));      
-    }  
-   
-   
+    }
 
-   
->>>>>>> cd558fa00a6bf9e64cf29145fe043feadbd1a86a
 }
